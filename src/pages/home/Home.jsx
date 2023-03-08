fe00e0df52b3bff3742624873acbfee2ac269732
jaeyeonp <wodusqkr1210@gmail.com>
2023-03-08 11:21:49 -0500

    123

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from '../../components/navbar/Navbar.jsx';
 import Header from '../../components/header/Header';
 import Featured from '../../components/featured/Featured';
 import PropertyList from '../../components/propertyList/PropertyList';
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
 
 const Home = () => {
   return (
@@ -14,6 +15,8 @@ const Home = () => {
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList/>
+        <h1 className="homeTitle">Stay at our top properties</h1>
+        <FeaturedProperties/>
       </div>
     </div>
   )
